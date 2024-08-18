a096e4f9569fa641e882890644f2ad7138c8211b
AndrefGuirro <131619465+AndrefGuirro@users.noreply.github.com>
2024-08-18 04:00:38 -0300

    Exercício 1
    aprendendo

---
+++ semana01.js
@@ -0,0 +1,32 @@
+
+    
+   scrrip <p>Digite um número inteiro positivo e clique no botão para verificar se ele é par ou ímpar:</p>
+    
+    
+    
+function solicitarNumero() {
+    let numero;
+
+    do {
+        numero = prompt("Por favor, digite um número inteiro positivo:");
+        
+        // Tente converter o número em inteiro
+        numero = parseInt(numero, 10);
+        
+        // Verifique se o número é um inteiro positivo
+        if (Number.isInteger(numero) && numero > 0) {
+            break;
+        } else {
+            alert("Entrada inválida. Por favor, digite um número inteiro positivo.");
+        }
+    } while (true);
+
+    // Verifica se o número é par ou ímpar
+    if (numero % 2 === 0) {
+        alert("O número " + numero + " é par.");
+    } else {
+        alert("O número " + numero + " é ímpar.");
+    }
+}
+
+solicitarNumero();
