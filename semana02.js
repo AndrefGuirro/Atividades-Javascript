7b9c0f6d295c541268dc42c970840d53062638e7
AndrefGuirro <131619465+AndrefGuirro@users.noreply.github.com>
2024-08-27 21:13:18 -0300

    função relogio
    função relógio

--- semana02.js
+++ semana02.js
@@ -22,3 +22,20 @@ document.addEventListener('DOMContentLoaded', () => {
     const dataAtual = new Date();
     dataAtualElement.textContent = formatarData(dataAtual);
 });
+
+function atualizarRelogio() {
+    const agora = new Date();
+
+    const horas = agora.getHours().toString().padStart(2, '0');
+    const minutos = agora.getMinutes().toString().padStart(2, '0');
+    const segundos = agora.getSeconds().toString().padStart(2, '0');
+
+    const horarioFormatado = `${horas}:${minutos}:${segundos}`;
+    
+    document.getElementById("relogio").textContent = horarioFormatado;
+
+    
+    setTimeout(atualizarRelogio, 1000);
+}
+
+atualizarRelogio();
